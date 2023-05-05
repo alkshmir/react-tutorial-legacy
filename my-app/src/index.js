4c0a5f57a72b03920cd53d586f83494b2e0bd045
alkshmir <shirakam.itsuk252@gmail.com>
2023-05-05 16:19:02 +0900

    react 完全に理解した

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -44,47 +44,60 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((sq, idx) => {
+            const desc = idx ?
+                'Go to move #' + idx :
+                'Go to game start';
+            return (
+                <li key={idx}>
+                    <button onClick={() => this.jumpTo(idx)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
-        if (winner){
+        if (winner) {
             status = 'Winner: ' + winner;
-        }else{
+        } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board 
+                    <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice(); // make copy of state.squares
-        if (calculateWinner(squares) || squares[i]){ // 勝者が決まっている or ますが埋まっている時に早期return
+        if (calculateWinner(squares) || squares[i]) { // 勝者が決まっている or ますが埋まっている時に早期return
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -92,7 +105,15 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(idx){
+        this.setState({
+            stepNumber: idx,
+            xIsNext: (idx % 2) === 0,
         });
     }
 }
